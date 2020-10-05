a51769c34807451be196e45c809fbca9888ea2c8
ASUS <ASUS@LAPTOP-2JE99TTR>
2020-10-06 00:20:55 +0700

    5

---
+++ SFML_GAME/Source.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(1080, 720), "Candy Runner");
+
+
+	////// Texture
+	sf::Texture playerTexture;
+	if (!playerTexture.loadFromFile("image/666.png"))
+	{
+		std::cout << "Load failed" << std::endl;
+	}
+	int spriteSizeX = playerTexture.getSize().x / 9;
+	int spriteSizeY = playerTexture.getSize().y / 6;
+	////// Sprite
+	sf::Sprite shapeSprite;
+	shapeSprite.setTexture(playerTexture);
+	shapeSprite.setTextureRect(sf::IntRect(0, 0, 50, 50));
+	int animationFrame = 0;
+	while (window.isOpen())
+	{
+		window.draw(shapeSprite);
+		window.display();
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		{
+			shapeSprite.move(.1f, 0.f);
+			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 1, spriteSizeX, spriteSizeY));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		{
+			shapeSprite.move(-.1f, 0.f);
+			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 3, spriteSizeX, spriteSizeY));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		{
+			shapeSprite.move(0.f, -.1f);
+			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, 0, spriteSizeX, spriteSizeY));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		{
+			shapeSprite.move(0.f, .1f);
+			shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 3, spriteSizeX, 50));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		window.clear();
+	}
+	return 0;
+}
+
+
--- SFML_GAME/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "SuperMarioGame.h"
-
-int main(int argc, char* argv[])
-{
-	CMarioGame::instance()->run();
-	return 0;
-}
-
