c0493278a2ebcc6bf2214196fd114c8e03f9a892
ASUS <ASUS@LAPTOP-2JE99TTR>
2020-10-19 11:11:26 +0700

    1

--- SFML_GAME/Source.cpp
+++ SFML_GAME/Source.cpp
@@ -1,19 +1,20 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include "Animation.h"
-
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1960, 1080), "Candy Runner",
-        sf::Style::Resize | sf::Style::Close);
+        sf::Style::Titlebar | sf::Style::Close);
     window.setVerticalSyncEnabled(true);
+    ///พื้นหลัง
 
-    sf::Texture t;
-    if (!t.loadFromFile("image/city.png"))
+    sf::Texture bg;
+    if (!bg.loadFromFile("image/city.png"))
         return EXIT_FAILURE;
-    t.setRepeated(true);
-    sf::Sprite background(t);
-    background.setPosition(0, 0);
+    bg.setRepeated(true);
+
+    sf::Sprite sprite(bg);
+    sprite.setPosition(0, 0);
+
 
     sf::Shader parallaxShader;
     parallaxShader.loadFromMemory(
@@ -27,30 +28,30 @@ int main()
         "}"
         , sf::Shader::Vertex);
 
-    sf::RectangleShape player(sf::Vector2f(100, 118));
-    player.setPosition(30, 780);
-    sf::Texture playerTexture;
-    if (!playerTexture.loadFromFile("image/game-sprite01.png"))
-        return EXIT_FAILURE;
-    player.setTexture(&playerTexture);
-
-    Animation animation(&playerTexture, sf::Vector2u(10,6), 0.5f);
-
-    float deltaTime = 0.0f;
-    sf::Clock clock2;
-
     float offset = 0.f;
+
     sf::Clock clock;
 
+    sf::Texture playerTexture;
+    if (!playerTexture.loadFromFile("image/game-sprite01.png"))
+    {
+        std::cout << "Load failed" << std::endl;
+    }
+    ////// Sprite
+    sf::Sprite shapeSprite;
+    shapeSprite.setTexture(playerTexture);
+    int spriteSizeX = playerTexture.getSize().x / 10;
+    int spriteSizeY = playerTexture.getSize().y / 6;
 
-
-
+    sf::Vector2u spriteSize = playerTexture.getSize();
+    int animationFrame = 0;
+    shapeSprite.setTextureRect(sf::IntRect(0, 0, 100, 118));
+    shapeSprite.setPosition(20, 765);
 
 
 
     while (window.isOpen())
     {
-        deltaTime = clock2.restart().asSeconds();
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -59,28 +60,35 @@ int main()
                 window.close();
                 break;
             }
-
         }
-        
 
+        ///รับค่าคีย์
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            shapeSprite.move(1.f, 0.f);
+            shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 5, spriteSizeX, spriteSizeY));
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        {
+            shapeSprite.move(0.f, 1.f);
+            shapeSprite.setTextureRect(sf::IntRect(spriteSizeX * animationFrame, spriteSizeY * 2, spriteSizeX, spriteSizeY));
+        }
 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+        {
+            window.close();
+        }
+        animationFrame++;
 
+        if (animationFrame >= 6) {
+            animationFrame = 0;
+        }
 
-
-
-
-
-
-
-
-        parallaxShader.setUniform("offset", offset += clock.restart().asSeconds() /20);
-
-        animation.Update(0, deltaTime,false);
-        player.setTextureRect(animation.uvRect);
-
+        ///คำลั่งวาดแสดงผลผ่านหน้าจอ
+        parallaxShader.setUniform("offset", offset += clock.restart().asSeconds() / 10);
         window.clear();
-        window.draw(background, &parallaxShader);
-        window.draw(player);
+        window.draw(sprite, &parallaxShader);
+        window.draw(shapeSprite);
         window.display();
     }
 
